ebaee165189a9ca178ba671059f511fb15407dcf
zabdielcead <zabdiel.cead@gmail.com>
2018-12-28 13:50:06 -0600

    suben ciclo de vida

--- directivas/src/app/app.component.ts
+++ directivas/src/app/app.component.ts
@@ -12,3 +12,14 @@ export class AppComponent {
     console.log('constructor entro super genial', this.prueba);
   }
 }
+
+/*
+ngOnInit cuando el componente esta inicializado
+ngOnChanges cuando la data de propiedades relacionadas cambian
+ngDoCheck Durante cada revision  del ciclo  de deteccion de cambios
+ngAftercontentInit despues de insertar contenido (<app-pagina>)
+ngAfterContentChecked despues de la revision del contenito insertado
+ngAfterViewInit despues de la inicializacion del componente/hijos
+ngAfterViewChecked despues de cada revision  de los componentes hijos
+ngOnDestroy justo antes que se destruya el componente o la directiva
+*/
